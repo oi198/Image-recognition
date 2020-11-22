c34b078cdf4d723ca373095e818461111d06c692
Ikuya Okamoto <67414951+oi198@users.noreply.github.com>
2020-11-23 00:28:26 +0900

    Update image_fit.py

--- image_fit.py
+++ image_fit.py
@@ -1,11 +1,10 @@
+import numpy as np
 from keras.models import load_model
+from google.colab.patches import cv2_imshow
+from PIL import Image
 from google.colab import drive
 drive.mount('/content/drive')
 
-
-import numpy as np
-from google.colab.patches import cv2_imshow
-from PIL import Image
 
 file = '/content/drive/My Drive/ファイル名.PNG'
 img = Image.open(file)
@@ -17,4 +16,5 @@ img = np.reshape(img, [28, 28, 1])
 
 model = load_model("/content/drive/My Drive/model.h5")
 img = np.array([img])
-model.predict_classes(img)
+ans = model.predict_classes(img)[0]
+print('your_number : ', ans)
